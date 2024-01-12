9c62ae8603317aac363afe31aed18fc7e1e8959d
Nicolai Langfeldt <nicolai.langfeldt@schibsted.no>
2024-01-12 11:01:08 +0100

    Updatify to new APIs

--- registry-count.py
+++ registry-count.py
@@ -1,33 +1,42 @@
 #!/usr/bin/env python3
 #
+# (C) 2024, Nicolai Langfeldt, Schibsted Products and Technology
+#
 # Script to garbage collect our (VGs) docker-registry by comparing the
 # running kubernetes with the things in the registry.
 #
 # Prerequisites:
 #
 
-from registryevictor import get_repositories, get_tags, spinner_next
-
-REGISTRY="docker.vgnett.no"
+import argparse
+import Registry
+import Spinner
 
 def main():
+    parser = argparse.ArgumentParser(description='Count number of tags in registry')
+    parser.add_argument('server', help='Registry server')
+    args = parser.parse_args()
+
+    spinner = Spinner.Spinner()
+
+    spinner.next()
+
     all_tags = []
-
-    spinner_next()
-
     num_repos = 0
 
-    for repo_name in get_repositories():
+    reg = Registry.Registry(args.server)
+
+    for repo_name in reg.get_repositories():
         num_repos += 1
-        spinner_next()
-        tags = get_tags(repo_name)
+        spinner.next()
+        tags = reg.get_tags(repo_name)
         if tags is None or len(tags) == 0:
             continue
         
         all_tags.extend(tags)
 
         print("  Tags 'til now: %d  " % len(all_tags), end="\r", flush=True)
-    # Print number of tags
+
     print("Number of repositories: %d, tags: %d" % (num_repos, len(all_tags)))
 
 if __name__ == "__main__":
