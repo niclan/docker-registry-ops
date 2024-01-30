472b67a942639d3c6a05b92a559b66083f0b626f
Nicolai Langfeldt <nicolai.langfeldt@schibsted.no>
2024-01-30 16:29:57 +0100

    Make more robust
    And use the shell wrapper to update all of the reports

--- container-start.sh
+++ container-start.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Run inventory script every 15 minutes
-cron.py -i 900 -d k8s-inventory.py
+cron.py -i 900 -d registry-checker.sh
 
-# This is the container anchor script. It should not terminate
-exec su www-data --shell=/bin/sh -c "exec /app/webserver.py"
+# And once at startup
+registry-checker.sh &
+
+# This is the container anchor. It should not terminate
+exec su www-data --shell=/bin/sh -c "while true; do /app/webserver.py; sleep 1; done"
