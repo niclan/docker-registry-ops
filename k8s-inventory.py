8468dd00ad064b0aca87a1b896cdf0896dab0f66
Nicolai Langfeldt <nicolai.langfeldt@schibsted.no>
2024-02-04 11:09:36 +0100

    Further work to elliminate old pods from report
    And some doc and reformating

--- k8s-inventory.py
+++ k8s-inventory.py
@@ -7,14 +7,12 @@
 # Copyright (C) 2024, Nicolai Langfeldt, Schibsted Products and Technology
 #
 # Prerequisites:
-# - apt install python3-kubernetes
-#
-# Usage:
-# - Edit the "us" regular expression to match your registry, this
-#   will be used to only save images from your registry to the images.json
-#   file.
+# - apt install python3-kubernetes (or pip install kubernetes)
 # - Have good credentials for all your clusters.
 # - Log in to AWS if you have cluster(s) there
+#
+# Usage:
+# - k8s-inventory.py
 #
 # Bugs:
 # - Could use a common configuration file with the registryevictor.py
@@ -27,26 +25,75 @@ import re
 import sys
 import json
 import pprint
+import argparse
 from kubernetes import client, config
+from datetime import datetime, timezone
 from kubernetes.client import configuration
 from kubernetes.client.rest import ApiException
 from kubernetes.config.config_exception import ConfigException
 
 def load_from_kubernetes(k8s, context=None):
+    """Load image list from all the pods in all the namespaces in the
+    given cluster.  The images and some status information are saved
+    in the global images dictionary.
+
+    Pods that are not running or pending and more than 31 days old
+    will be ignored.
+    """
+
+    global too_old
+
     # Kubernetes pod phases:
     # https://kubernetes.io/docs/concepts/workloads/pods/pod-lifecycle/#pod-phase
     #
     # ImagePullBackOff is not a kubernetes phase, but for the registry-checker it's
-    # interestng
+    # interesting
     no_phase = { 'Running': False, 'Pending': False, 'Succeeded': False, \
                  'Failed':  False, 'Unknown': False, 'ImagePullBackOff': False }
     count = 0
+
+    # Each container can be the origin of several different pods.  A
+    # long running pod, a cronJob pod etc.  Therefore the outer key is
+    # the container since we're all about the container images really.
     
     for i in k8s.list_pod_for_all_namespaces(watch=False).items:
         
         if i.status.container_statuses is None: continue
 
+        # This is the namespace-point of suffcient specificity to save
+        # if the registry tag is running somewhere and where that is.
+        pod_name = f'k8s;{context};{i.metadata.namespace};{i.metadata.name}'
+
+        if i.metadata.name == 'elton-services-syncing-transaction-job-27963550-5gx5r':
+            print("Found it")
+
+        # Figure out when this pod was last wanted
+        if i.status.phase in ['Pending', 'Running'] or ipbo:
+            pod_age = 0 # now
+        else:
+            pod_age = (datetime.now(timezone.utc) - i.status.start_time).total_seconds()
+            pod_age = pod_age / 60 / 60 / 24
+
         for c in i.status.container_statuses:
+            ipbo = c.state.waiting is not None and \
+                c.state.waiting.reason == 'ImagePullBackOff'
+
+            c_age = pod_age
+
+            if c.state.running is not None:
+                c_age = 0 # now
+            elif pod_age > 0 and c.state.terminated is not None:
+                # Get the launch time of the container if it's terminated,
+                # and then ignore it if it's older than max_age
+                c_age = datetime.now(timezone.utc) - c.state.terminated.started_at
+                c_age = c_age.total_seconds() / 60 / 60 / 24
+                if c_age > max_age:
+                    # print("Skipping %s, too old at %d days" % (c.image, c_age) )
+                    too_old += 1
+                    continue
+
+                # print("Container %s in pod %s is not running and is %d days old" % (c.name, i.metadata.name, c_age))
+
             count += 1
             if c.image not in images:
                 images[c.image] = { '_phase': no_phase.copy() }
@@ -56,26 +103,36 @@ def load_from_kubernetes(k8s, context=None):
 
             images[c.image]['_phase']['ImagePullBackOff'] = ipbo
 
-            # We don't need to know the state of all the
-            # individual pods, but we do need to know if the image
-            # is used in a running pod. This is the
-            # namespace-point of suffcient specificity to save if
-            # the registry tag is running somewhere and where that
-            # is.
-            pod_name = f'k8s;{context};{i.metadata.namespace};{i.metadata.name}'
-
+            # If we haven't seen this container/pod combination
+            # before, make a new emtpy phase summary for it.
             if pod_name not in images[c.image]:
                 images[c.image][pod_name] = no_phase.copy()
 
+                    # The outer nesting is the image - because we're concerned with the images
             images[c.image][pod_name][i.status.phase] = True
             images[c.image][pod_name]['ImagePullBackOff'] = ipbo
+            images[c.image][pod_name]['_last_wanted'] = c_age
+
             images[c.image]['_phase'][i.status.phase] = True
-                
+
+            if '_last_wanted' not in images[c.image] or \
+               images[c.image]['_last_wanted'] > c_age:
+                images[c.image]['_last_wanted'] = c_age
+
     print("* Found %s pods" % count)
-    print("* Images until now: %d" % len(images))
+    print("* Images until now: %d, and %d are too old" % (len(images), too_old))
 
 
 def main():
+    parser = argparse.ArgumentParser(description='Collect docker image inventory from kubernetes')
+    parser.add_argument('-a', '--age', action='store', type=int, default=31, \
+                        help='Only inlucde images younger than this many days, default is 31')
+    args = parser.parse_args()
+
+    global max_age, too_old
+    max_age = args.age
+    too_old = 0
+
     try:
         contexts, active_context = config.list_kube_config_contexts()
         print("Loaded contexts from kube-config file")
