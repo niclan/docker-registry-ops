7cba6f62bdcb4fcef5f8f8016a4c224313ebee12
Nicolai Langfeldt <nicolai.langfeldt@schibsted.no>
2024-04-16 14:08:48 +0200

    Add command to delete images

---
+++ registry-rm.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+#
+# (C) 2024, Nicolai Langfeldt, Schibsted Products and Technology
+#
+# List tags in a docker registry.
+#
+
+import sys
+import socket
+import requests
+import argparse
+import Registry
+
+def main():
+    parser = argparse.ArgumentParser(description='Delete registry tags')
+    parser.add_argument('-s', '--server', help='Registry server')
+    parser.add_argument('image', action='append', nargs='?', help='Image(s) to delete')
+    args = parser.parse_args()
+
+    ntags = 0
+    num_repos = 0
+
+    try:
+        reg = Registry.Registry(args.server)
+
+    except requests.exceptions.ConnectionError:
+        print("Failed to connect to %s" % args.server)
+        sys.exit(1)
+
+    reg.do_delete = True
+    reg.verbose = True
+
+    for image_tag in args.image:
+
+        if "@" in image_tag:
+            (repository, digest) = image_tag.split("@")
+
+        (repository, tag) = image_tag.split(":")
+        digest, _, _ = reg.get_manifest(repository, tag)
+
+        print(f"Deleting {repository}:{digest}")
+
+        reg.delete_manifest(repository, digest)
+        
+if __name__ == "__main__":
+    main()
